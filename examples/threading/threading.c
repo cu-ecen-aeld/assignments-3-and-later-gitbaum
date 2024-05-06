48fc751465cba9e443e3132cb82aefd74f24d7cc
Tobias Baumgartner (HC-CS/PAC-IOT) <tobias.baumgartner2@de.bosch.com>
2024-05-07 00:31:24 +0200

    assignement 4 part 1 finished

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -4,9 +4,16 @@
 #include <stdio.h>
 
 // Optional: use these functions to add debug or error prints to your application
-#define DEBUG_LOG(msg,...)
-//#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
+//#define DEBUG_LOG(msg,...)
+#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
 #define ERROR_LOG(msg,...) printf("threading ERROR: " msg "\n" , ##__VA_ARGS__)
+
+void sleep_ms(int ms) {
+  struct timespec ts;
+  ts.tv_sec = ms / 1000;
+  ts.tv_nsec = (ms % 1000) * 1000000;
+  nanosleep(&ts, NULL);
+}
 
 void* threadfunc(void* thread_param)
 {
@@ -14,7 +21,18 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
-    return thread_param;
+
+  struct thread_data *td = (struct thread_data*)thread_param;
+  
+  DEBUG_LOG("Started THREAD");
+  sleep_ms(td->wait_to_obtain_ms);
+  pthread_mutex_lock(td->mutex);
+  sleep_ms(td->wait_to_release_ms);
+  pthread_mutex_unlock(td->mutex);
+
+  td->thread_complete_success = true;
+  
+  return thread_param;
 }
 
 
@@ -28,6 +46,16 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
+  struct thread_data *td = (struct thread_data*)malloc(sizeof(struct thread_data));
+  td->mutex = mutex;
+  td->wait_to_obtain_ms = wait_to_obtain_ms;
+  td->wait_to_release_ms = wait_to_release_ms;
+  
+  int rc = pthread_create(thread, NULL, threadfunc, td);
+  if (rc != 0) {
+    ERROR_LOG("Failed to create thread");
     return false;
+  }
+  return true;
 }
 
